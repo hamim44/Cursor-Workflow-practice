2be1bffe196285c3b34e3e773a58d9140a2c77a5
Abrar hamim <abrarhamim4044h@gmail.com>
2025-07-14 01:09:24 +0600

    Initial Commit

---
+++ Cursor Workflow/Cursor_WorkflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cursor_WorkflowApp.swift
+//  Cursor Workflow
+//
+//  Created by Abrar Hamim on 14/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Cursor_WorkflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
